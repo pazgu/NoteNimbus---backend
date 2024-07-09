a7104fd64494eb1a21c91a1e25ff285a6ffc5133
Paz Gueta <pazgueta@gmail.com>
2024-07-09 22:05:21 +0300

    fixed deleting image handling

--- routers/note.route.js
+++ routers/note.route.js
@@ -20,10 +20,10 @@ const router = express.Router();
 
 router.get("/:userId/:id", authorizeNoteOwner, getNoteById);
 router.get("/:userId", authorizeOwnerDetails, getUserNotes);
-router.post("/create", upload.single("image"), createNote);
+router.post("/create", upload.single("image"), createNote); //middleware used for handling single image upload
 router.put("/:userId/:id", authorizeNoteOwner, editNote);
+router.delete("/:id/image", authorizeNoteOwner, deleteImage);
 router.delete("/:userId/:id", authorizeNoteOwner, deleteNote);
 router.patch("/:userId/:id", authorizeNoteOwner, toggleIsPinned);
-router.delete("/:id/image", authorizeNoteOwner, deleteImage);
 
 module.exports = router;
